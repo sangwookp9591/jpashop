c9ad0997680fdcdfe27fcb52175a47ca8a6eb566
psw <sangwookp9591@gmail.com>
2021-03-05 15:39:34 +0900

    스프링 부트와 JPA 활용1 -DAY5 상품 주문

--- src/main/java/jpabook/jpashop/controller/OrderController.java
+++ src/main/java/jpabook/jpashop/controller/OrderController.java
@@ -10,6 +10,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -32,4 +34,33 @@ public class OrderController {
         return "order/orderForm";
 
     }
+
+
+    @PostMapping("/order")
+    public String order(@RequestParam("memberId") Long memberId,
+                        @RequestParam("itemId") Long itemId,
+                        @RequestParam("count") int count){
+        //@RequestParam은 form submit 방식이다.여기선 선택된 selectBox의 value값과 count값이 매핑되어 바인딩된다.
+
+
+        orderService.order(memberId, itemId, count);
+        // 해당값을 controller에도 설정해도되지만 하지 않는 이유는
+        //컨트롤러에서 설정하게되면 controller자체가 지저분해질 뿐 만 아니라,
+        //테스트할때도 그렇고 transcation안에서 JPA가 동작할때 제일깔끔하게 동작하기 때문이다.
+        //controller에서는 식별자만 넘기고 service 계층에서는 엔티티랑 이런것을 더 의존하기 떄문에
+        //안에서 넘기면 더할 수 있는게 많다.
+        //주로 command성 주문이런것은 Controller에서 식별자만 넘기고 실제 핵심 비지니스 서비스에서 enity를 찾는것부터는 거기서한다
+        //enity의 값들도 transcation 안에서 조회를해야 영속상태로 진행되기 때문에 그럼 상태도 변경할 수 있다.
+
+        //조회는 상관없지만 조회가 아닌 핵심 비지니스로직은 밖에서 넘기는것보다 식별자만 받은뒤 핵심비지니스 로직을 transcaction안에서 할경우
+        //영속상태를 유지할 수 있기때문에 주문하면서 뭔가 수정되더라도 변경감지를 적용가능 밖에서 가지고오면 더티체킹 자체가 안됨
+        //트랜잭션없이 밖에서 들고온거기때문에.
+        return "redirect:/orders";
+
+
+
+
+
+    }
+
 }
